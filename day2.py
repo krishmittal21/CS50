04b1f05edb57e75edf979bec67b5fb1e1569aa4d
krishmittal21 <114004042+krishmittal21@users.noreply.github.com>
2022-10-13 19:35:07 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,13 @@
+#whether x is positive using functions
+def main():
+    x=int(input("What's x? "))
+    if is_even(x):
+        print("x is an even number ")
+    else:
+        print("x is not even")
+def is_even(number1):
+    if number1 %2==0:
+        return True
+    else:
+        return False   
+main()
